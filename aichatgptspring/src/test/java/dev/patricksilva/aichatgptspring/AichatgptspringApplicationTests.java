a08395a5fc92b72fcb3ce333de787019de6a2d51
patricksilva1 <patricksilva5160@gmail.com>
2023-01-14 20:25:09 -0300

    feat: :sparkles: Creating the project

---
+++ aichatgptspring/src/test/java/dev/patricksilva/aichatgptspring/AichatgptspringApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.patricksilva.aichatgptspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AichatgptspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
